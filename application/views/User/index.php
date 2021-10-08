dbbed97b55a306e0c86f52a4293d36f858ab4b1a
Handriwati04 <handriaty1003@gmail.com>
2021-10-08 23:30:46 +0700

    second

---
+++ application/views/User/index.php
@@ -0,0 +1,210 @@
+</html>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <title>Portfolio</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-primary">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Cuma Satu Halaman Cukup Kan!*_* <span class="sr-only">(current)</span></a>
+                </li>
+
+        </div>
+    </nav><br><br><br><br>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/12/17/1554607248.jpg" class="rounded mx-auto d-block " style="width: 500px">
+            </div>
+            <div class="carousel-item">
+                <img src="https://1.bp.blogspot.com/-XR91qsSJkTE/Woq8Z25k3eI/AAAAAAAAACA/wQF9ktgeyK4w3UkyrwNHpy4DJZY2-DNpgCLcBGAs/s1600/Gambar-Pemandangan-Alam-Danau-1194.jpg" class="rounded mx-auto d-block " style="width: 500px">
+            </div>
+            <div class="carousel-item">
+                <img src="https://p4.wallpaperbetter.com/wallpaper/848/338/475/blossoms-colors-flowers-nature-wallpaper-preview.jpg" class="rounded mx-auto d-block " style="width: 500px">
+            </div>
+        </div>
+    </div><br><br><br><br>
+
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Nama</th>
+                <th scope="col">Tanggal_Lahir</th>
+                <th scope="col">No_Hp</th>
+                <th scope="col">Email</th>
+                <th scope="col">Alamat</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Handriwati</td>
+                <td>25-04-00</td>
+                <td>085918772453</td>
+                <td>handriwati782@gmail.com</td>
+                <td>Kumbang selatan,Desa Kumbang</td>
+            </tr>
+        </tbody>
+    </table>
+    <br><br><br><br>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h6 class="text-center">About:</h6>
+                <a>Nama saya handriwati! Saya anak terakhir dari 4 bersaudara,
+                    saat ini saya masih menempuh pendidikan di perguruan tinggi Universitas Hamzanwadi,
+                    Jurusan:Teknik informatika,
+                    Saat ini saya magang di salah satu perusahaan Karye Kanak Desa(Kdd).
+                </a>
+            </div>
+            <div class="col order-2">
+                <img src="assets/img/friends.jpg" style="width: 500px">
+            </div>
+            <div class="col order-3">
+                <h6 class="text-center">Motivasi</h6>
+                <p class="text-lowercase">Orang Tua</p>
+                <p class="text-uppercase">Orang Lain</p>
+                <p class="text-capitalize">Diri Sendiri</p>
+            </div>
+        </div>
+    </div>
+    <br><br><br><br>
+    <div class="text-center">
+        <div class="spinner-grow text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-secondary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-success" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-danger" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-warning" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-info" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-light" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-dark" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-secondary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-success" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-danger" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-warning" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-info" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-light" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-dark" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-secondary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-success" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-danger" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-warning" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-info" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-light" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <div class="spinner-grow text-dark" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    </div>
+    </div>
+    <br><br><br><br><br>
+    <div class="container">
+        <div class="row">
+            <div class="col order-last">
+
+            </div>
+            <div class="col-MT-2">
+                "PENDIDIKAN"
+            </div>
+            <div class="col order-first">
+
+            </div>
+        </div>
+    </div>
+    <div class="text-center col-mt-2">
+        <a class="list-group-item list-group-item-action">SDN 3 LENDANG NANGKA</a>
+        <a class="list-group-item list-group-item-action">SMPN 2 MASBAGIK</a>
+        <a class="list-group-item list-group-item-action">SMK NW KUMBANG</a>
+        <a class="list-group-item list-group-item-action">SAAT INI MASIH SEBAGAI MAHASISWI DI UNIVERSITAS HAMZANWADI</a>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+    <div class="card text-center">
+        <div class="card-body" style="background-color: #808080;">
+            <h5 class="card-title">Sedikit cerita </h5>
+            <p class="card-text">Munkin Hanya ini untuk portfolionya *_*!</p>
+            <a href="kontak.php" class="btn btn-dark">Keep Smiling and enjoy your life simply</a>
+        </div>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
